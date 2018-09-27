ed3e5ad7065000f00b39c88136c61ec08d51fb7e
Roland4444 <turbopollux@rambler.ru>
2018-09-27 13:59:27 +0000

    first commit

---
+++ src/main/scala/Executor.scala
@@ -0,0 +1,16 @@
+package zerber.co.uk;
+import java.sql._
+import java.sql.DriverManager.getConnection
+class Executor(ip: String, db: String, login: String, passwd: String, Usejtds: Boolean) {
+  var cnnct = "jdbc:sqlserver://" + ip + "\\MSSQLSERVER:1433;database=" + db + ";user=" + login + ";password=" + passwd
+  if (Usejtds)
+    cnnct = "jdbc:jtds:sqlserver://" + ip + ":1433/" + db + ";instance=MSSQLSERVER;user=" + login + ";password=" + passwd
+  val conn = getConnection(cnnct)
+
+  def submit(query: String): ResultSet = {
+    val stmt = conn.createStatement
+    val res = stmt.executeQuery(query)
+    res
+  }
+}
+
---
+++ src/test/java/DB/ExecutorTest.java
@@ -0,0 +1,38 @@
+package DB;
+import org.junit.Test;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import static org.junit.Assert.*;
+public class ExecutorTest {
+    zerber.co.uk.Executor exec = new zerber.co.uk.Executor("192.0.0.14", "ODAY", "roman", "00000", true);
+
+    public ExecutorTest() throws SQLException {
+    }
+
+    @Test
+    public void insert() throws SQLException {
+        ResultSet result =  exec.submit("select * from dbo.banks");
+        ResultSetMetaData rm = result.getMetaData();
+        while (result.next()){
+            for (int i=1; i<=rm.getColumnCount(); i++)
+                System.out.println(result.getString(i));
+        }
+    }
+
+    @Test
+    public void getColumn() throws SQLException {
+        ResultSet result =  exec.submit("select * from dbo.banks");
+        ResultSetMetaData rm = result.getMetaData();
+        assertEquals(rm.getColumnName(1), "f_key");
+        assertTrue(rm.getColumnCount()>1 );
+        System.out.println(rm.getColumnCount());
+        for (int i = 1; i <=rm.getColumnCount(); i++)
+            System.out.println(rm.getColumnName(i));
+
+    }
+
+    @Test
+    public void insert1() {
+    }
+}
