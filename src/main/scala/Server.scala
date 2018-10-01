6d11804a78e922a2f2f4f7723685e95816ec0d13
Roland4444 <turbopollux@rambler.ru>
2018-10-01 16:20:34 +0000

    ws ready, ajax testing

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,22 +1,39 @@
+import java.util.function.Consumer
+
+import io.javalin.websocket.{ConnectHandler, WsHandler}
 import io.javalin.{Context, Handler, Javalin}
 object Server{
   val JSONSQL: JSONSQL=new JSONSQL
   val rootHandler: Handler = new Handler {override def handle(context: Context) ={context.render("fake.html")      }}
+  val loadIndex: Handler = new Handler {override def handle(context: Context) ={context.render("index.html")      }}
+
   val postHandler: Handler = new Handler {override def handle(context: Context) ={
     context.req.getParameter("query")
     val output = new StringBuffer
     output.append(JSONSQL.getJSonfromQuery(context.req.getParameter("query")))
     context.html(output.toString)}
   }
-  val testHandler: Handler = new Handler {override def handle(context: Context) ={context.html("yey")     }}
+  val testHandler: Handler = new Handler {override def handle(context: Context) ={
+    print( context.req.)
+
+    println(context.mapFormParams().size())
+    println(context.mapQueryParams().size())
+    context.html("yey")   }}
+
 
 
   def main(args: Array[String]): Unit = {
     var app: Javalin = Javalin.create.start(7777)
     app.get("/", rootHandler)
     app.get("/test", testHandler)
-    app.post("/", postHandler)
+    app.get("/aj", testHandler)
+    app.get("/load", loadIndex)
+
+
   }
+
+
+
 }
 
 
