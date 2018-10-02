e6e9b516371c55529fd2514c16125df8af8f4757
Roland4444 <turbopollux@rambler.ru>
2018-10-02 14:59:11 +0000

    BRITISH POUND;; adding cookie support, redirecting

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,6 +1,3 @@
-import java.util.function.Consumer
-
-import io.javalin.websocket.{ConnectHandler, WsHandler}
 import io.javalin.{Context, Handler, Javalin}
 object Server{
   val JSONSQL: JSONSQL=new JSONSQL
@@ -18,14 +15,39 @@ object Server{
   val testHandler: Handler = new Handler {override def handle(context: Context) ={
     print( context.formParam("name"))
     context.html(context.formParam("name"))   }}
+  val initHandler: Handler = new Handler {override def handle(context: Context) ={context.redirect("/redirected")}}
+  val redirectedHandler: Handler = new Handler {override def handle(context: Context) ={context.html("redirected")}}
+  val cockieHandler: Handler = new Handler {override def handle(context: Context) ={
+    if (context.req.getCookies!=null){
+      val cookie = context.req.getCookies
+      cookie.foreach(println(_))
+    }
+    else context.redirect("/login")
+
+""
+  }}
+  val loginHandler: Handler = new Handler {override def handle(context: Context) ={context.render("login.html") }}
+  val postloginHandler: Handler = new Handler{override def handle(context: Context) ={
+    val login = context.formParam("login")
+    val passwd = context.formParam("passwd")
+    context.cookie("login", login)
+    context.cookie("passwd", passwd)
+  }}
+
+
   def main(args: Array[String]): Unit = {
     var app: Javalin = Javalin.create.start(7777)
+    app.get("/cookie", cockieHandler)
     app.get("/", rootHandler)
     app.post("/test", testHandler)
     app.post("/aj", testHandler)
     app.get("/load", loadIndex)
     app.post("/submit", postHandler)
     app.get("/ws", websocket)
+    app.get("/login", loginHandler)
+    app.get("/init", initHandler)
+    app.post("/login", postloginHandler)
+    app.get("/redirected", redirectedHandler)
     app.ws("/ws",  ws => {
       ws.onConnect(session => System.out.println("Connected"));
       ws.onMessage((session, message) => {
